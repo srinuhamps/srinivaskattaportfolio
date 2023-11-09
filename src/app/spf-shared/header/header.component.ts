d446dc5d89b2b39c786b6d6ee20f6b4f26298e6e
Srinivas Reddy Katta <kattasrinivas7988@gmail.com>
2023-11-09 12:11:28 -0600

    links update

--- src/app/spf-shared/header/header.component.ts
+++ src/app/spf-shared/header/header.component.ts
@@ -9,11 +9,19 @@ import { Router } from '@angular/router';
 export class HeaderComponent {
   @HostListener('window:scroll', ['$event'])
   onScroll(e: any) {
-    console.log('window', e);
+    const wrap = document.getElementById('wrap') as HTMLElement;
+
+    wrap.addEventListener('scroll', function (e) {
+      if (this.scrollTop > 147) {
+        wrap.classList.add('fix-search');
+      } else {
+        wrap.classList.remove('fix-search');
+      }
+    });
   }
 
   constructor(private router: Router) {}
-  loadModule(path: string) {
-    this.router.navigate([path]);
+  loadModule(link: boolean, path: string) {
+    link ? window.open(path, '_blank') : this.router.navigate([path]);
   }
 }
